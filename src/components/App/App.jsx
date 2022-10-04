62996cad943494d388f88d92557508119d1b9c96
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-10-04 18:37:01 +0300

    remove default contacts. fix contacts useState

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,20 +6,12 @@ import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/Filter';
 import { HeadingPrimary } from 'components/App/AppStyled';
 
-const defaultContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 const LS_CONTACTS_KEY = 'contacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = JSON.parse(localStorage.getItem(LS_CONTACTS_KEY));
-    return savedContacts?.length > 0 ? savedContacts : defaultContacts;
-  });
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LS_CONTACTS_KEY)) ?? []
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
